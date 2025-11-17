10834594537eabfdf269128fa2da3e9c49fc4bd5
Benard Atika <benardmachuka08@gmail.com>
2025-11-17 21:03:03 +0300

    Delete client directory

--- client/src/hooks/useSocket.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { SocketContext } from '../context/SocketContext.jsx';
-
-const useSocket = () => useContext(SocketContext);
-
-export default useSocket;
-
-
-
